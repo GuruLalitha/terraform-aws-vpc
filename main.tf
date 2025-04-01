4cf5890d027c033d119f524c0051b0d9ff10546d
GuruLalitha <guru.lalitha90@gmail.com>
2025-04-01 15:21:20 +0530

    terraform

--- main.tf
+++ main.tf
@@ -140,6 +140,7 @@ resource "aws_route" "public" {
   gateway_id = aws_internet_gateway.main.id
 }
 
+
 resource "aws_route" "private" {
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = "0.0.0.0/0"
